98453ca77928fa6fa4b136c99e5d7487cb99fff6
heitor6697 <36076016+heitor6697@users.noreply.github.com>
2019-10-18 17:55:27 -0300

    Update controleDeAgendamentos.php

--- motor/control/controleDeAgendamentos.php
+++ motor/control/controleDeAgendamentos.php
@@ -8,9 +8,9 @@
         error_reporting(E_ALL ^ E_NOTICE ^ E_STRICT);
     }
     session_start();
-    /*if(empty($_SESSION)){
+    if(empty($_SESSION)){
         header("location: ../../authentication/login.php");
-    }*/
+    }
 
     $id_usuario=$_REQUEST['id_usuario'];
     $descricao = $_REQUEST['descricao'];
