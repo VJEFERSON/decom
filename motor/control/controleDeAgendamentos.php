ada46ffcb4dc1d1d24b67fc51a3b30726361e121
heitor6697 <36076016+heitor6697@users.noreply.github.com>
2019-10-23 17:31:01 -0300

    Update controleDeAgendamentos.php

--- motor/control/controleDeAgendamentos.php
+++ motor/control/controleDeAgendamentos.php
@@ -11,19 +11,32 @@
     if(empty($_SESSION)){
         header("location: ../../authentication/login.php");
     }
-
+    //$codigo_objeto=$_REQUEST['objeto_editar'];
+    //$data=$_REQUEST['data_editar'];
+   // $horario=$_REQUEST['horario_editar'];
+    $codobj=$_REQUEST['idobj'];
+    $codhor=$_REQUEST['idhor'];
+    $datage=$_REQUEST['iddatage'];
     $id_usuario=$_REQUEST['id_usuario'];
     $descricao = $_REQUEST['descricao'];
     $id_objeto=$_REQUEST['id_objeto'];
     $datas_agendamento=$_REQUEST['datas_agendamento'];
     $id_horarios=$_REQUEST['id_horarios'];
     $acao_formulario = $_REQUEST['acao_formulario'];
+    $action = $_REQUEST['action'];
     $_SESSION['respostaDaRequisicao']='erro';
 	$agendamento = new Agendamento();
     
     if($acao_formulario == 'deletar-agendamento'){
-        var_dump($id_agendamento);
-    } else {
+        $resposta = $agendamento->deletarAgendamento($codobj, $codhor, $datage);
+        
+        if ($resposta === NULL) {
+            $_SESSION['respostaDaRequisicao']='deletar-sucesso';	
+        } else {
+         $_SESSION['respostaDaRequisicao']='deletar-erro';	
+        }
+        header("location: ../../system-pages/agendamento.php");
+        } else {
         switch($acao_formulario) {
             case 'criar-agendamento':
                 
@@ -39,9 +52,18 @@
                 break;	
     
             case 'editar-agendamento':
-                
-                break;			
+                $agendamento->setarValoresDaInstancia($id_objeto, $id_usuario, $id_horarios, $datas_agendamento, $descricao, '0');
+                $resposta = $agendamento->atualizarValoresDoAgendamento($codobj, $datage, $codhor);
+                if($resposta === NULL){
+                    $_SESSION['respostaDaRequisicao']='editar-sucesso';
+                }   
+                else{
+                    $_SESSION['respostaDaRequisicao']='editar-erro';
+                }
+                header("location: ../../system-pages/agendamento.php");
+                break;	
+
         }
-    }
+        }
     //header("location: ../../system-pages/agendamento.php");
 ?>
